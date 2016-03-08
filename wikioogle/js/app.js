0ae54f7f3db962a0654de33cf6ecdafdfd46a225
Alistair Martin <alistairmartin01@gmail.com>
2016-03-08 22:18:31 +1030

    [wikioogle] Add ajax calls to Wikipedia, and update search results

--- wikioogle/js/app.js
+++ wikioogle/js/app.js
@@ -1,3 +1,51 @@
 $(document).ready( function() {
-  // do something
+  var resultHTML = '<div class="result"><h3><a href="{0}">{1}</a></h3>' +
+    '<div class="search-content"><cite class="search-url">{2}</cite><p>{3}</p></div></div>';
+
+  var noMatch = '<div class="result"><div class="no-result"><p>Your search - ' +
+    '<strong>{0}</strong> - did not match any documents.</p><p>Suggestions:</p>' +
+    '<ul><li>Make sure all words are spelled correctly.</li><li>Try different keywords.</li>' +
+    '<li>Try more general keywords.</li><li>Try fewer keywords.</li></ul></div></div>';
+
+  var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search={0}&callback=?";
+
+  $('#search-text').keyup( function () {
+    var query = $(this).val();
+    searchWikipedia(query);
+  });
+
+  function searchWikipedia(query) {
+    $.getJSON( url.format(query), function(json) {
+
+      // remove existing results
+      $('.result').each( function() {
+        $(this).remove();
+      });
+
+      // add new results to the DOM
+      var result, results = json.slice(1);
+      console.log(results[0].length);
+      if (results[0].length >= 1) {
+        for (var i = 0; i < results[0].length; i++) {
+          var cite = results[2][i].replace("https://", "");
+          result = resultHTML.format(results[2][i], results[0][i], cite, results[1][i]);
+        }
+      } else {
+        result = noMatch.format(query);
+      }
+      $('.results').append(result);
+    });
+  }
 });
+
+String.prototype.format = function() {
+  // replaces {index} in the string with arguments provided.
+  // eg. "hello{0}".format(" world"); returns "hello world"
+  // numbers between the brackets must correspond with the argument index
+  var args = arguments.length === 0? undefined: Array.prototype.slice.call(arguments, 0);
+  var result = this;
+  args.forEach( function(val, i) {
+    result = result.replace("{" + i + "}", val);
+  });
+  return result;
+};
