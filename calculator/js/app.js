072264e980c085ab594b8f3b1f58f2eb4cd693b7
Alistair Martin <alistairmartin01@gmail.com>
2016-03-17 14:52:49 +1030

    [calc] Bring buttons to life

---
+++ calculator/js/app.js
@@ -0,0 +1,35 @@
+$(document).ready( function() {
+  var expression = '0';
+  var clickActions = { 'AC': clearAll, 'C': clearLastChar, '=': evaluateExpression };
+
+  $('button').click( function(e) {
+    var val = e.currentTarget.value;
+    if (val in clickActions) { clickActions[val](); }
+    else { addToExpression(val); }
+  });
+
+  function addToExpression(val) {
+    if (expression === '0') { expression = val; }
+    else if (!!expression.slice(-1).match( /\/|\*|\+|\%|-/ ) &&
+             !!val.match( /\/|\*|\+|\%|-/ )) {
+      expression = expression.slice(0, -1) + val;
+    }
+    else { expression += val; }
+    $('.display p').text(expression);
+  }
+
+  function clearLastChar() {
+    expression = expression.length === 1? '0' : expression.slice(0, -1);
+    $('.display p').text(expression);
+  }
+
+  function clearAll() {
+    expression = '0';
+    $('.display p').text(expression);
+  }
+
+  function evaluateExpression() {
+    // TODO: evaluate the expression!
+    $('.display p').text(expression);
+  }
+});
