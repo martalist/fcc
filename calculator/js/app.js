88e9c8691158793569671f3b787f777808f27155
Alistair Martin <alistairmartin01@gmail.com>
2016-03-17 20:36:28 +1030

    [calc] Perform arithmetic/calculations

--- calculator/js/app.js
+++ calculator/js/app.js
@@ -39,7 +39,47 @@ $(document).ready( function() {
   }
 
   function evaluateExpression() {
-    // TODO: evaluate the expression!
+    expArray = expression.match( /\d+(\.d+)?|(?:\+|-|\/|\*|\%)/g );
+    expression = '' + solve(expArray);
     $('.display p').text(expression);
+  }
+
+  function solve(arr) {
+    var newArr = arr.slice(0);
+    var precedence = ['%', '/', '*', '-', '+'];
+    for (var i = 0; i < precedence.length; i++) {
+      var match = newArr.indexOf(precedence[i]);
+      while (match > -1) {
+        if (precedence[i] === '-') {
+          newArr[match] = '+';
+          newArr[match + 1] = '-' + newArr[match + 1];
+        }
+        else {
+          var group = calculate(newArr.splice(match - 1, 3));
+          newArr.splice(match - 1, 0, group);
+        }
+        match = newArr.indexOf(precedence[i]);
+      }
+    }
+    return newArr[0];
+  }
+
+  function calculate(arr) {
+    arr[0] = parseFloat(arr[0]);
+    arr[2] = parseFloat(arr[2]);
+    switch (arr[1]) {
+      case '/':
+        return arr[0] / arr[2];
+      case '*':
+        return arr[0] * arr[2];
+      case '+':
+        return arr[0] + arr[2];
+      case '-':
+        return arr[0] - arr[2];
+      case '%':
+        return arr[0] % arr[2];
+      default:
+        throw Error('That\'s not an operator!');
+    }
   }
 });
