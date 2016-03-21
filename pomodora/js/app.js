fc5165aec1c41b09c068e57873495f2653167eae
Alistair Martin <alistairmartin01@gmail.com>
2016-03-21 13:23:33 +1030

    [pomodora] Create a basic timer

--- pomodora/js/app.js
+++ pomodora/js/app.js
@@ -1,3 +1,19 @@
 $(document).ready( function() {
-  // do something
-};
+  // start clock
+  $('.clock').click(function() {
+    // calculate # of seconds
+    var seconds = $(this).text() * 60;
+    // while remaining sec > 0
+    var timer = setInterval( function() {
+      seconds--;
+      var minRemaining = Math.floor(seconds / 60);
+      var secRemaining = seconds % 60;
+      secRemaining = (secRemaining / 10 < 1) ? '0' + secRemaining : secRemaining;
+      $('.clock').text(minRemaining + ':' + secRemaining);
+      if (seconds === 0) {
+        // TODO: trigger next timer (break or session)
+        clearInterval(timer);
+      }
+    }, 1000);
+  });
+});
