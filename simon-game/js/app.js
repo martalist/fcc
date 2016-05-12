5f41cf1e406f59e517c659067dbb8065c4a129f1
Alistair Martin <alistairmartin01@gmail.com>
2016-05-12 14:00:45 +0930

    [simon] Animate indicators, play sounds

--- simon-game/js/app.js
+++ simon-game/js/app.js
@@ -6,6 +6,7 @@ var Game = function(strict=false) {
   this.sequence = [];
   this.userCorrectCount = 0;
   this.rounds = 20;
+  this.progress = [];
   this.presentingSequence = false;
   this.speed = 1000;
   this.msgBoard = document.getElementById('msg');
@@ -14,10 +15,17 @@ var Game = function(strict=false) {
     start: document.getElementById('start'),
     restart: document.getElementById('restart')
   };
+  this.sounds = {
+    monkey: new Audio('files/monkey.mp3'),
+    dog: new Audio('files/dog.mp3'),
+    pig: new Audio('files/pig.mp3'),
+    elephant: new Audio('files/elephant.mp3')
+  };
 };
 
 Game.prototype.startNewGame = function() {
   this.sequence = [];
+  this.removeIndicators();
   this.userCorrectCount = 0;
   this.levelUp();
 };
@@ -34,6 +42,9 @@ Game.prototype.levelUp = function() {
     this.addToSequence();
     // display new level
     this.level.innerText = this.sequence.length;
+    this.resetIndicators();
+    this.addIndicator();
+
     this.animateElements(this.sequence, true);
   }
 };
@@ -53,8 +64,10 @@ Game.prototype.animateElements = function(elems, isSequence=false) {
   if (isSequence) { this.presentingSequence = true; }
   var wait,
       arr = elems.slice(0),                         // copy array, to prevent sequence mutation
-      elem = arr.shift();                           // get first elem
-  elem.classList.toggle('active');                  // add class
+      elem = arr.shift(),                           // get first elem
+      animal = elem.id;
+  elem.classList.toggle('active');
+  this.sounds[animal].play();
   wait = setTimeout( function(game, arr) {          // pause, then remove it
     elem.classList.toggle('active');
     if (arr.length === 0) {
@@ -75,12 +88,17 @@ Game.prototype.checkInput = function(input) {
   // check that user input matches next item in sequence
   var correct = this.sequence[this.userCorrectCount] === input;
   if (correct) {
-    // TODO: show a success indicator (like a gree thumbs up, or something)
+    // Show a success indicator
+    this.progress[this.userCorrectCount].classList.add('correct');
+
     // check if user successfully reached the end of the sequence
     if (this.userCorrectCount === this.sequence.length - 1) {
-      this.userCorrectCount = 0;
-      var pause = setTimeout(function(game) {
-        game.levelUp();
+      this.userCorrectCount = 0;          // reset the counter
+      var resetIndicators = setTimeout( function(game) {
+        game.resetIndicators();           // reset the visual success indicators
+      }, this.speed * 1.5, this);
+      var levelUp = setTimeout(function(game) {
+        game.levelUp();                   // progress to the next level
       }, this.speed * 2.5, this);
     }
     else {
@@ -90,6 +108,8 @@ Game.prototype.checkInput = function(input) {
   else {
     // TODO: notify user of incorrect input
     this.showMessage('That is incorrect');
+    this.resetIndicators();
+    this.userCorrectCount = 0;
     var pauseToLetTheirFailureSinkIn = setTimeout(function(game) {
       if (game.strict) { this.startNewGame(); }
       else { game.animateElements(game.sequence, true); }
@@ -102,6 +122,29 @@ Game.prototype.showMessage = function(msg) {
   var remove = setTimeout( function(msgBoard) {
     msgBoard.innerText = '';
   }, this.speed * 2, this.msgBoard);
+};
+
+Game.prototype.addIndicator = function() {
+  var i = document.createElement('div');
+  i.classList.add('progress');
+  document.getElementsByClassName('progress-bar')[0].appendChild(i);
+  this.progress.push(i);
+  var oneMoment = setTimeout(function() {
+    i.classList.add('inserted');
+  }, 10);
+};
+
+Game.prototype.resetIndicators = function() {
+  this.progress.forEach( function(elem) {
+    elem.classList.remove('correct');
+  });
+};
+
+Game.prototype.removeIndicators = function() {
+  while (this.progress.length > 0 ) {
+    var elem = this.progress.pop();
+    elem.parentNode.removeChild(elem);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', function() {
