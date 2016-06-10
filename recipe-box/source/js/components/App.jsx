d6affee880a493c73569aae860c059483f4f3ae1
Alistair Martin <alistairmartin01@gmail.com>
2016-06-10 18:18:17 +0930

    [recipebox] Save recipes to the browser's localStorage

--- recipe-box/source/js/components/App.jsx
+++ recipe-box/source/js/components/App.jsx
@@ -2,26 +2,43 @@ import React, { Component } from 'react';
 import Header from './Header.jsx';
 import RecipeListContainer from './RecipeListContainer.jsx';
 import recipes from '../utils/recipes';
+import { storageAvailable } from '../utils/helpers';
 
 class App extends Component {
   constructor() {
     super();
     this.state = {
       recipes,
+      localStorage: false
     };
     this.updateRecipeList = this.updateRecipeList.bind(this);
     this.deleteRecipe = this.deleteRecipe.bind(this);
+  }
+  componentDidMount() {
+    if (storageAvailable('localStorage')) {
+      this.setState({localStorage: true}, () => {
+        const localData = localStorage.getItem('fccRecipeList'),
+              recipes = JSON.parse(localData) || this.state.recipes;
+        this.updateStateStorage({recipes});
+      });
+    }
   }
   updateRecipeList(updatedRecipe, index) {
     const recipes = this.state.recipes.slice();
     index = index < 0 ? recipes.length : index;
     recipes[index] = updatedRecipe;
-    this.setState({recipes: recipes});
+    this.updateStateStorage({recipes});
   }
   deleteRecipe(index) {
     const recipes = this.state.recipes.slice();
     recipes.splice(index, 1);
-    this.setState({recipes: recipes});
+    this.updateStateStorage({recipes});
+  }
+  updateStateStorage(obj) {
+    this.setState(obj);
+    if (this.state.localStorage) {
+      localStorage.setItem('fccRecipeList', JSON.stringify(obj.recipes));
+    }
   }
   render() {
     const props = {
--- recipe-box/source/js/utils/helpers.js
+++ recipe-box/source/js/utils/helpers.js
@@ -11,3 +11,16 @@ export const findRecipe = (arr, urlParam) => arr.reduce(
   (a,b) => titleToUrl(b.name) === urlParam ? b : a,
   null
 );
+
+export const storageAvailable = (type) => {
+	try {
+		var storage = window[type],
+			x = '__storage_test__';
+		storage.setItem(x, x);
+		storage.removeItem(x);
+		return true;
+	}
+	catch(e) {
+		return false;
+	}
+};
